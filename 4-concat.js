2822e1ca770854dd95a22f8211ad2e517a34ef29
John Linus <johnlinus467@gmail.com>
2025-09-21 13:07:59 +0100

    4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the first and second arguments after node and the script name
+const first = process.argv[2];
+const second = process.argv[3];
+
+// Print in the format: "first is second"
+console.log(`${first} is ${second}`);
